269bc3e3ddc0cf44fb6c3bf4fbd024712d383fdf
Leonardo T. Fernandes <leo.80@protonmail.com>
2020-11-29 09:48:48 -0300

    add styles to dashboard using styled-components

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,16 @@
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
 
+import GlobalStyle from './styles/global';
 import Routes from './routes/index';
 
 const App: React.FC = () => (
+  <>
     <BrowserRouter>
-        <Routes />
+      <Routes />
     </BrowserRouter>
+    <GlobalStyle />
+  </>
 );
 
 export default App;
--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,7 +1,60 @@
 import React from 'react';
+import { FiChevronRight } from 'react-icons/fi';
+
+import logoImg from '../../assets/logo.svg';
+
+import { Title, Form, Repositories } from './styles';
 
 const Dashboard: React.FC = () => {
-    return <h1>Dashboard</h1>;
+  return (
+    <>
+      <img src={logoImg} alt="logo" />
+      <Title>Explore repositórios no Github</Title>
+
+      <Form>
+        <input type="text" placeholder="Digite o nome do repositório" />
+        <button type="submit">Pesquisar</button>
+      </Form>
+
+      <Repositories>
+        <a href="teste">
+          <img
+            src="https://avatars2.githubusercontent.com/u/60330828?s=460&u=cc32102f0ee41d53cdbb248f7d592e2312b331c2&v=4"
+            alt="avatar"
+          />
+          <div>
+            <strong>titulo do repo</strong>
+            <p>descrição do repo</p>
+          </div>
+          <FiChevronRight size={20} />
+        </a>
+
+        <a href="teste">
+          <img
+            src="https://avatars2.githubusercontent.com/u/60330828?s=460&u=cc32102f0ee41d53cdbb248f7d592e2312b331c2&v=4"
+            alt="avatar"
+          />
+          <div>
+            <strong>titulo do repo</strong>
+            <p>descrição do repo</p>
+          </div>
+          <FiChevronRight size={20} />
+        </a>
+
+        <a href="teste">
+          <img
+            src="https://avatars2.githubusercontent.com/u/60330828?s=460&u=cc32102f0ee41d53cdbb248f7d592e2312b331c2&v=4"
+            alt="avatar"
+          />
+          <div>
+            <strong>titulo do repo</strong>
+            <p>descrição do repo</p>
+          </div>
+          <FiChevronRight size={20} />
+        </a>
+      </Repositories>
+    </>
+  );
 };
 
 export default Dashboard;
