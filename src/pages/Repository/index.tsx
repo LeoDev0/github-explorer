72c0f1ff0daeac132076d1c6eaa7f6ea24da8e15
Leonardo T. Fernandes <leo.80@protonmail.com>
2020-11-29 19:13:29 -0300

    add styles to repositories details page

--- src/pages/Repository/index.tsx
+++ src/pages/Repository/index.tsx
@@ -1,5 +1,10 @@
 import React from 'react';
-import { useRouteMatch } from 'react-router-dom';
+import { useRouteMatch, Link } from 'react-router-dom';
+import { FiChevronLeft, FiChevronRight } from 'react-icons/fi';
+
+import logoImg from '../../assets/logo.svg';
+
+import { Header, RepositoryInfo, Issues } from './styles';
 
 interface RepositoryParams {
   repository: string;
@@ -8,7 +13,54 @@ interface RepositoryParams {
 const Repository: React.FunctionComponent = () => {
   const { params } = useRouteMatch<RepositoryParams>();
 
-  return <h1>Repository:{params.repository}</h1>;
+  return (
+    <>
+      <Header>
+        <img src={logoImg} alt="logo" />
+        <Link to="/dashboard">
+          <FiChevronLeft size={16} />
+          Voltar
+        </Link>
+      </Header>
+
+      <RepositoryInfo>
+        <header>
+          <img
+            src="https://avatars2.githubusercontent.com/u/60330828?s=460&u=cc32102f0ee41d53cdbb248f7d592e2312b331c2&v=4"
+            alt="avatar"
+          />
+          <div>
+            <strong>blablabla</strong>
+            <p>Desceição</p>
+          </div>
+        </header>
+        <ul>
+          <li>
+            <strong>18010</strong>
+            <span>Stars</span>
+          </li>
+          <li>
+            <strong>18010</strong>
+            <span>Forks</span>
+          </li>
+          <li>
+            <strong>18010</strong>
+            <span>Issues abertas</span>
+          </li>
+        </ul>
+      </RepositoryInfo>
+
+      <Issues>
+        <Link to="dfgsdfsdf">
+          <div>
+            <strong>uijokplḉ;</strong>
+            <p>ghyjukiolpç</p>
+          </div>
+          <FiChevronRight size={20} />
+        </Link>
+      </Issues>
+    </>
+  );
 };
 
 export default Repository;
